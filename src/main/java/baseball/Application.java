b0ff5a6946768ffbdb26e13c24f29856cec002a3
yeonzeop <ksp2617@naver.com>
2025-01-26 13:50:14 +0900

    char version

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,7 +13,7 @@ public class Application {
         menu=1;
         try {
             while (menu != 2) {
-                baseball();
+                baseball2();
                 if (menu != 2) {
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                     menu = Integer.parseInt(Console.readLine());
@@ -25,7 +25,38 @@ public class Application {
             throw e;
         }
     }
+    private static void baseball2(){
+        List<Character> key=new ArrayList<>();
 
+        while(key.size()<size){
+            int num=Randoms.pickNumberInRange(1,9);
+            char c=(char)(num+'0');
+            if(!key.contains(c))
+                key.add(c);
+        }
+
+        while(true) {
+            try {
+                System.out.print("숫자를 입력해주세요 : ");
+                String str = Console.readLine();
+                if (str.length() != size) throw new IllegalArgumentException("");
+                char[] chars=str.toCharArray(); //흐음
+                List<Character> answer=new ArrayList<>();
+                for(int i=0;i<size;i++){
+                    if(answer.contains(chars[i])||chars[i]<'1'||chars[i]>'9')
+                        throw new IllegalArgumentException("");
+                    answer.add(chars[i]);
+                }
+                if(checkForGame2(key, answer)) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
+                }
+            } catch (IllegalArgumentException e) {
+                throw e;
+            }
+        }
+
+    }
     private static void baseball() {
         List<Integer> key=new ArrayList<>();
 
@@ -45,7 +76,7 @@ public class Application {
                 List<Integer> answer = new ArrayList<>();
                 for (int i = 0; i < size; i++) {
                     Integer num = Integer.parseInt(strs[i]);
-                    if (answer.contains(num) || num < 1 || num > 9)
+                    if (answer.contains(num) || num < 1 || num > 9) //질문
                         throw new IllegalArgumentException("");
                     answer.add(num);
                 }
@@ -59,7 +90,38 @@ public class Application {
         }
 
     }
+    private static boolean checkForGame2(List<Character> key,List<Character>answer){
+        int strike=0;
+        int ball=0;
 
+        for(int i=0;i<size;i++){
+            Character num=answer.get(i);
+            for(int j=0;j<size;j++){
+                Character keyNum=key.get(j);
+                if(keyNum.equals(num)){
+                    if(j==i)
+                        strike++;
+                    else ball++;
+                    break;
+                }
+            }
+        }
+        String str="";
+        if(strike==0&&ball==0){
+            System.out.println("낫싱");
+            return false;
+        } else if (ball>0||strike>0) {
+            if(ball>0){
+                str+=ball+"볼 ";
+            }
+            if(strike>0){
+                str+=strike+"스트라이크";
+            }
+            System.out.println(str);
+        }
+        if(strike==3) return true;
+        else return false;
+    }
     private static boolean checkForGame(List<Integer> key, List<Integer> answer) {
         int strike=0;
         int ball=0;
